207d334be54fea1cb382ac79e5fd9f6d9cf05bcd
Micah Cooper <mc@micahcooper.com>
2016-07-12 17:00:12 -0400

    working on my files

--- javascript/micah.js
+++ javascript/micah.js
@@ -1,8 +1,18 @@
 function testMe()
 {
 	console.log( "hi" );
-	$('#calendar').fullCalendar({
+	var myCalendar = $('#calendar').fullCalendar(
+	{
         // put your options and callbacks here
+		events:
+		[
+			{
+				title: 'Failing Successfully',
+				start: '2016-07-14',
+				description: 'A presentation on the philosophy of how failing can change your life'
+			}
+		]
     });
-	console.log( $('#calendar') );
+	
+	console.log( myCalendar );
 }
