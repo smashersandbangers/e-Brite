6069663352e0739bf2cb99298192572425c04ea6
Micah Cooper <mc@micahcooper.com>
2016-07-11 23:03:23 -0400

    small changes

--- javascript/events-database.js
+++ javascript/events-database.js
@@ -1,27 +1,56 @@
-var preloadedEvents = [{
-		"eventid": "01",
-		"eventName": 'Success Through Failures',
+var preloadedEvents = [
+	{
+		'eventid': '01',
+		'eventName': 'Success Through Failures',
 		'eventLocation': 'Atlanta',
 		'eventDate': '4/22/2017',
 		'eventSeats': '8',
-		'eventCurrentAttendees': '8'
-		'eventList':
-		{
+		'eventCurrentAttendees': '8',
+		'eventList':[
+			{
 			'firstName': 'John',
 			'lastName': 'Smitty'
-		}
-	}, {
+			},
+			{
+			'firstName': 'Sue',
+			'lastName': 'Mee'
+			}
+		]
+	},
+	{
 		"eventid": "02",
 		'eventName': 'Successfully Failing',
 		'eventLocation': 'Boston',
 		'eventDate': '4/24/2017',
 		'eventSeats': '10',
-		'eventCurrentAttendees': '7'
-	}, {
+		'eventCurrentAttendees': '7',
+		'eventList':[
+			{
+			'firstName': 'John',
+			'lastName': 'Smitty'
+			},
+			{
+			'firstName': 'Sue',
+			'lastName': 'Mee'
+			}
+		]
+	},
+	{
 		"eventid": "03",
 		'eventName': 'To Fail or Not to Fail',
 		'eventLocation': 'Boston',
 		'eventDate': '4/24/2017',
 		'eventSeats': '10',
-		'eventCurrentAttendees': '33'
-	}];
+		'eventCurrentAttendees': '33',
+		'eventList':[
+			{
+			'firstName': 'John',
+			'lastName': 'Smitty'
+			},
+			{
+			'firstName': 'Sue',
+			'lastName': 'Mee'
+			}
+		]
+	}
+];
