d63d910a173f22199551d3d20b55ffc0a6c7e609
Micah Cooper <mc@micahcooper.com>
2016-07-12 21:31:16 -0400

    adding jquery-ui, event details

--- javascript/micah.js
+++ javascript/micah.js
@@ -3,26 +3,61 @@ function testMe()
 	console.log( "hi" );
 	var myCalendar = $('#calendar').fullCalendar(
 	{
-        weekends: false,
+        header:
+		{
+			left: 'month,agendaWeek,agendaDay'
+		},
+		height: 500,
+		editable: true,
+		durationEditable: true,
+		weekends: false,
 		events:
 		[
 			{
 				title: 'Failing Successfully',
-				start: '2016-07-14',
+				start: '2016-07-14 08:00',
 				description: 'A presentation on the philosophy of how failing can change your life'
 			}
 		],
 		dayClick: function()
 		{
 			//alert('a day has been clicked!');
+			console.log("clicked");
+			switchToDay();
 		},
-		eventRender: function(event, element) {
-			console.log( element );
-			element.qtip({
-				content: event.description
+		eventResize: function(event, dayDelta, minuteDelta, revertFunc){
+            updateEvent(event);
+        },
+		eventClick: function (event, jsEvent, view) {
+			//set the values and open the modal
+			console.log( event );
+			$("#eventInfo").html(event.description);
+			$("#eventLink").attr('href', event.url);
+			$("#eventContent").dialog({
+				modal: true,
+				title: event.title
 			});
+			return false;
 		}
     });
 	
 	console.log( myCalendar );
 }
+
+function switchToDay()
+{
+	
+}
+
+function updateEvent(the_event) {
+    $.update(
+      "/events/" + the_event.id,
+      { event: { title: the_event.title,
+                 starts_at: "" + the_event.start,
+                 ends_at: "" + the_event.end,
+                 description: the_event.description
+               }
+      },
+      function (reponse) { console.log('successfully updated task.'); }
+    );
+};
