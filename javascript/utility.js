ccd7bcff3276a8bea4516c407c0cc3a9e8123c10
Micah Cooper <mc@micahcooper.com>
2016-07-16 09:54:46 -0400

    big changes to calendar selection

--- javascript/utility.js
+++ javascript/utility.js
@@ -3,3 +3,53 @@ function log( logItem ){
 	if( debug )
 		console.log( logItem );
 }
+
+//add the calendar names to the drop down list
+function loadApplication()
+{
+	saveCalendarEvents( "events", preLoadedEvents );
+	
+	addCalendarsToNavBar();
+	
+	var myCalendar = jQuery('#calendar').fullCalendar(
+	{
+        header:
+		{
+			left: 'month,agendaWeek,agendaDay'
+		},
+		height: 500,
+		editable: false,
+		durationEditable: true,
+		weekends: false,
+		events: getCalendarEvents("events"),
+		dayClick: function()
+		{
+			log("clicked");
+			switchToDay();
+		},
+		eventResize: function(event, dayDelta, minuteDelta, revertFunc){
+            updateEvent(event);
+        },
+		eventClick: function (event, jsEvent, view) {
+			//set the values and open the modal
+			console.log( jsEvent );
+			jQuery("#eventInfo").html(event.description);
+			jQuery("#eventStartTime").html("<p>"+moment(event.start).format()+"</p>");
+			jQuery("#eventAttendees").html("<p>"+event.attendees[0].firstName+"</p>");
+			jQuery("#eventLink").attr('href', 'modify-event-micah.html?id='+event.eventid);
+			jQuery("#eventContent").dialog({
+				modal: false,
+				title: event.title
+			});
+			return false;
+		}
+    });
+}
+
+function addCalendarsToNavBar()
+{
+	log("cal nav bar");
+	log( jQuery('#calendarList') );
+	
+	jQuery('#calendarList').append( new Option('demo events','events') );
+}
