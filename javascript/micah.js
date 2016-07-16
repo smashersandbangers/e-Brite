27ff36ce13a3cbc474f09f1337c2c376ff5e7c19
Micah Cooper <mc@micahcooper.com>
2016-07-16 09:56:27 -0400

    working on changes for storage interaction

--- javascript/micah.js
+++ javascript/micah.js
@@ -1,51 +1,13 @@
 //let's get this started
 function testMe()
 {
-	storeEvents( "events", preLoadedEvents );
-	
-	var myCalendar = $('#calendar').fullCalendar(
-	{
-        header:
-		{
-			left: 'month,agendaWeek,agendaDay'
-		},
-		height: 500,
-		editable: false,
-		durationEditable: true,
-		weekends: false,
-		//referencing my preloaded events variable
-		events: getEventsMicah("events"),
-		dayClick: function()
-		{
-			//alert('a day has been clicked!');
-			console.log("clicked");
-			switchToDay();
-		},
-		eventResize: function(event, dayDelta, minuteDelta, revertFunc){
-            updateEvent(event);
-        },
-		eventClick: function (event, jsEvent, view) {
-			//set the values and open the modal
-			console.log( jsEvent );
-			$("#eventInfo").html(event.description);
-			$("#eventStartTime").html("<p>"+moment(event.start).format()+"</p>");
-			$("#eventAttendees").html("<p>"+event.attendees[0].firstName+"</p>");
-			$("#eventLink").attr('href', 'modify-event-micah.html?id='+event.eventid);
-			$("#eventContent").dialog({
-				modal: false,
-				title: event.title
-			});
-			return false;
-		}
-    });
-	
-	//console.log( myCalendar );
+
 	
 }
 
 //no idea what this does yet
 function updateEvent(the_event) {
-    $.update(
+    jQuery.update(
       "/events/" + the_event.id,
       { event: { title: the_event.title,
                  starts_at: "" + the_event.start,
@@ -88,8 +50,8 @@ function getCalendarMicah(calendarName)
 //adding a new event to the existing event calendar
 function addEventToCalendarMicah(calendarName)
 {
-	log( $('#newTitle') );
-	var newTitle = '{"title":"'+$('#newTitle')[0].value+'"}';
+	log( jQuery('#newTitle') );
+	var newTitle = '{"title":"'+jQuery('#newTitle')[0].value+'"}';
 	log( newTitle );
 	
 	var myCalendar = getCalendarEvents( calendarName );
